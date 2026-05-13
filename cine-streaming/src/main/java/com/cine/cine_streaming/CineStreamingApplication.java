41875a218aef085d4c0da44b0db4d23d4aefb400
Rhode <ur.dav3@gmail.com>
2026-05-13 07:01:36 +0100

    Premier commit: projet initial

---
+++ cine-streaming/src/main/java/com/cine/cine_streaming/CineStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.cine.cine_streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineStreamingApplication.class, args);
+	}
+
+}
---
+++ cine-streaming/src/test/java/com/cine/cine_streaming/CineStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cine.cine_streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
